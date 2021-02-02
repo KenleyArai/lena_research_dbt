4e47f26032e054d8261c2d16d08cff86aa2add9c
Kenley Arai <me@kenleyar.ai>
2021-02-01 16:45:19 -0800

    corrected baseline correction equation

--- models/eda/baseline_corrected_time_series.sql
+++ models/eda/baseline_corrected_time_series.sql
@@ -16,7 +16,7 @@ add_base_line_correction AS
         rts.eda_subject_id,
         rts.epoch,
         rts.series_type,
-        (rts.mean - qpa.channel_avg)::FLOAT/rts.mean::FLOAT AS baseline_correction
+        (rts.mean - qpa.channel_avg)::FLOAT AS baseline_correction_avg
     FROM
         {{ ref('raw_time_series') }} AS rts
             JOIN quiet_period_avg AS qpa ON
@@ -25,18 +25,30 @@ add_base_line_correction AS
     WHERE
         rts.is_quiet_period IS FALSE
 ),
-add_base_line_percent_change AS
+add_prev_baseline_correction AS
 (
     SELECT
         eda_subject_id,
         epoch,
         series_type,
-        baseline_correction,
-        (baseline_correction - LAG(baseline_correction) OVER (PARTITION BY eda_subject_id, series_type ORDER BY epoch))/LAG(baseline_correction) OVER (PARTITION BY eda_subject_id, series_type ORDER BY epoch) AS baseline_percent_change
+        baseline_correction_avg,
+        LAG(baseline_correction_avg) OVER (PARTITION BY eda_subject_id, series_type ORDER BY epoch) AS prev_baseline_correction_avg
     FROM add_base_line_correction
+),
+add_baseline_percent_change AS
+(
+    SELECT
+        eda_subject_id,
+        epoch,
+        series_type,
+        baseline_correction_avg,
+        prev_baseline_correction_avg,
+        (prev_baseline_correction_avg - baseline_correction_avg)::FLOAT/prev_baseline_correction_avg::FLOAT AS percent_change_from_baseline
+    FROM
+        add_prev_baseline_correction
 )
 
 SELECT
     *
 FROM
-    add_base_line_percent_change
+    add_baseline_percent_change
